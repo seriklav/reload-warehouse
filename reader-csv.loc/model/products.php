56ea495da1f636ee87d0ddc6c0e2f5dbc9408302
Serik <serik_lav@mail.ru>
2018-01-22 15:09:55 +0200

    Update products.php

--- reader-csv.loc/model/products.php
+++ reader-csv.loc/model/products.php
@@ -2,7 +2,7 @@
 	class ModelProducts extends Model{
 
 		public function getProducts(){
-			$products = $this->db->query("SELECT product_name, GROUP_CONCAT(DISTINCT warehouse) as warehouse, SUM(qty) as qty FROM `product` GROUP BY product_name HAVING qty > 0");
+			$products = $this->db->query("SELECT product_name, GROUP_CONCAT(DISTINCT  `warehouse`) as warehouse, SUM(`qty`) as qty FROM `product` GROUP BY product_name HAVING qty > 0");
 
 			return $products->rows;
 		}
@@ -15,15 +15,16 @@
 			else
 				return false;
 		}
+
 		public function addProduct($data = array()){
 			if($data['id']) {
 				$this->db->query("UPDATE `product` SET qty = '" . (int)$data['qty'] . "' WHERE id = '" . (int)$data['id'] . "'");
 				$id = $data['id'];
 			} else {
 				$this->db->query("INSERT INTO `product` 
-									SET qty = '" . (int)$data['qty'] . "', 
-									`product_name` = '" . $this->db->escape($data['product_name']) . "', 
-									`warehouse` = '" . $this->db->escape($data['warehouse']) . "' ");
+										SET qty = '" . (int)$data['qty'] . "', 
+										`product_name` = '" . $this->db->escape($data['product_name']) . "', 
+										`warehouse` = '" . $this->db->escape($data['warehouse']) . "' ");
 				$id = $this->db->getLastId();
 			}
 			return $id;
